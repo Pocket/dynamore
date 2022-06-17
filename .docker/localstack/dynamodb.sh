6aa6bfe77cf88f62dbbf953dd3298ef84c76b1bd
Kat Schelonka <kschelonka@mozilla.com>
2022-06-17 13:54:48 -0700

    test: composite key test + table

--- .docker/localstack/dynamodb.sh
+++ .docker/localstack/dynamodb.sh
@@ -2,14 +2,14 @@
 set -x
 
 TABLE_DEFINITIONS=(
-  'test-hash-table'
+  'test-hash-table' 'test-composite-table'
 )
 
 for json_file in "${TABLE_DEFINITIONS[@]}"; do
   # start fresh and delete the table if it exists
   awslocal dynamodb delete-table --table-name ${json_file} || true
   awslocal dynamodb create-table --cli-input-json file://$(dirname "${BASH_SOURCE[0]}")/dynamodb/${json_file}.json
-  awslocal dynamodb batch-write-item --request-items file://$(dirname "${BASH_SOURCE[0]}")/dynamodb/${json_file}-seed.json
+  # awslocal dynamodb batch-write-item --request-items file://$(dirname "${BASH_SOURCE[0]}")/dynamodb/${json_file}-seed.json
 done
 
 
--- test/get.integration.ts
+++ test/get.integration.ts
@@ -3,7 +3,8 @@ import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import { config } from './config';
 import { dynamore } from '../lib/dynamore';
 import { seed, truncateTable } from './utils';
-import { seedData } from './fixtures/test-hash-table-seed';
+import { seedData as hashSeed } from './fixtures/test-hash-table-seed';
+import { seedData as compositeSeed } from './fixtures/test-composite-table-seed';
 
 describe('DynamoreGet', () => {
   const client = new DynamoDBClient({
@@ -11,14 +12,18 @@ describe('DynamoreGet', () => {
     endpoint: config.aws.endpoint,
   });
   const dynamodb = DynamoDBDocumentClient.from(client);
-  const table = dynamore(dynamodb)('test-hash-table');
+  const hashTable = dynamore(dynamodb)('test-hash-table');
+  const compTable = dynamore(dynamodb)('test-composite-table');
+
   beforeAll(async () => {
     await truncateTable('test-hash-table', client);
-    await seed(client, seedData);
+    await seed(client, hashSeed);
+    await truncateTable('test-composite-table', client);
+    await seed(client, compositeSeed);
   });
   describe('find', () => {
     it('gets all available attributes for a key', async () => {
-      const res = await table.find({ id: 'abc123' }).send();
+      const res = await hashTable.find({ id: 'abc123' }).send();
       expect(res.Item).toEqual({
         id: 'abc123',
         name: 'dracula',
@@ -26,23 +31,41 @@ describe('DynamoreGet', () => {
         species: 'vampire',
       });
     });
-    it('returns undefined Item if key does not exist', async () => {
-      const res = await table.find({ id: 'notreal' }).send();
+    it('gets all available attributes for a composite key', async () => {
+      const res = await compTable.find({ name: 'goku', powerLevel: 10 }).send();
+      expect(res.Item).toEqual({
+        name: 'goku',
+        powerLevel: 10,
+        series: 'dragonball',
+        saga: 'Emperor Pilaf',
+        source: 'Daizenshuu 7',
+        form: 'human',
+      });
+    });
+    it('returns undefined Item if hash key does not exist', async () => {
+      const res = await hashTable.find({ id: 'notreal' }).send();
+      expect(res.Item).toBeUndefined;
+    });
+    it('returns undefined Item if composite key does not exist', async () => {
+      const res = await compTable.find({ name: 'goku', powerLevel: 0 }).send();
       expect(res.Item).toBeUndefined;
     });
     it('selects a specific attribute', async () => {
-      const res = await table.find({ id: 'hij789' }).select('species').send();
+      const res = await hashTable
+        .find({ id: 'hij789' })
+        .select('species')
+        .send();
       expect(res.Item).toEqual({ species: 'human' });
     });
     it('selects multiple attributes', async () => {
-      const res = await table
+      const res = await hashTable
         .find({ id: 'def456' })
         .select('species', 'name')
         .send();
       expect(res.Item).toEqual({ species: 'dhampir', name: 'alucard' });
     });
     it('returns undefined for nonexistent attributes', async () => {
-      const res = await table
+      const res = await hashTable
         .find({ id: 'def456' })
         .select('species', 'name', 'favoriteFood')
         .send();
@@ -54,8 +77,8 @@ describe('DynamoreGet', () => {
     });
   });
   describe('findMany', () => {
-    it('returns multiple keys and all their attributes', async () => {
-      const res = await table
+    it('returns multiple hash keys and all their attributes', async () => {
+      const res = await hashTable
         .findMany([{ id: 'hij789' }, { id: 'klm123' }])
         .send();
       expect(res.Items.length).toEqual(2);
@@ -70,8 +93,32 @@ describe('DynamoreGet', () => {
         species: 'human',
       });
     });
+    it('returns multiple composite keys and all their attributes', async () => {
+      const res = await compTable
+        .findMany([
+          { name: 'goku', powerLevel: 100 },
+          { name: 'vegeta', powerLevel: 18000 },
+        ])
+        .send();
+      expect(res.Items.length).toEqual(2);
+      expect(res.Items).toContainEqual({
+        name: 'goku',
+        powerLevel: 100,
+        series: 'dragonball',
+        saga: 'Emperor Pilaf',
+        source: 'Daizenshuu 7',
+        form: 'great ape',
+      });
+      expect(res.Items).toContainEqual({
+        name: 'vegeta',
+        powerLevel: 18000,
+        series: 'dragonball z',
+        saga: 'Vegeta',
+        source: 'Vol. 21, #249',
+      });
+    });
     it('returns multiple keys with selected attributes', async () => {
-      const res = await table
+      const res = await hashTable
         .findMany([{ id: 'def456' }, { id: 'klm123' }])
         .select('name', 'species')
         .send();
@@ -86,7 +133,7 @@ describe('DynamoreGet', () => {
       });
     });
     it('returns multiple keys with non-overlapping attributes', async () => {
-      const res = await table
+      const res = await hashTable
         .findMany([{ id: 'def456' }, { id: 'klm123' }])
         .select('name', 'species', 'aka')
         .send();
